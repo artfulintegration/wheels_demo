eebb5ee16a832a5ac85f5a147e80c2ee75126d26
Tyler Gannon <tgannon@gmail.com>
2010-11-01 22:10:08 -0700

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ WheelsDevelopment::Application.routes.draw do
   devise_for :users
   wheels_resources
 
-  root :to => 'pages#show', :level1=>"Welcome"
+  root :to => 'pages#show', :level1=>"Artful"
 
   match "/submit_feedback" => "feedbacks#new"
   match '/software' => 'product_revisions#index'
